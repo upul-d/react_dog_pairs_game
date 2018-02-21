432d59888f1a50051b0738ecccd1e1089be1051a
upuld <u.dissanayaka@gmail.com>
2018-02-21 13:37:22 +0000

    Hide card by default

--- dog_pairs/src/components/Card.js
+++ dog_pairs/src/components/Card.js
@@ -3,13 +3,29 @@ import React, {Component} from 'react';
 class Card extends Component {
   constructor(props) {
     super(props)
+
+    this.state = {
+      active: false
+    }
+
+    this.onCardClick = this.onCardClick.bind(this);
+  }
+
+  onCardClick() {
+    this.setState({ active: true});
+    this.props.handleCardClick(this.props.position);
   }
 
   render() {
+
     if (this.props.url === "x") {
       return <div className="card"></div>
-    } else {
-      return <div className="card" onClick={ () => {this.props.handleCardClick(this.props.position)} }><img src={this.props.url} /></div>
+    }
+    else if (!this.state.active) {
+      return <div className="card hidden" onClick={ this.onCardClick }></div>
+    }
+    else {
+      return <div className="card"><img src={this.props.url} /></div>
     }
   }
 
