48549424841f6e1c4abdea87fa6710d4f9521b75
upuld <u.dissanayaka@gmail.com>
2018-02-21 13:30:28 +0000

    Refactor Card to class

--- dog_pairs/src/components/Card.js
+++ dog_pairs/src/components/Card.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-const Card = (props) => {
-  if (props.url === "x") {
-    return <div className="card"></div>
-  } else {
-    return <div className="card" onClick={ () => {props.handleCardClick(props.position)} }><img src={props.url} /></div>
+class Card extends Component {
+  constructor(props) {
+    super(props)
   }
+
+  render() {
+    if (this.props.url === "x") {
+      return <div className="card"></div>
+    } else {
+      return <div className="card" onClick={ () => {this.props.handleCardClick(this.props.position)} }><img src={this.props.url} /></div>
+    }
+  }
+
 }
 
 export default Card;
