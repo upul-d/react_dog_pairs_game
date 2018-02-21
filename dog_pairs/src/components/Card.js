ae8f9721018287cb39797938f0687387ef4811c7
upuld <u.dissanayaka@gmail.com>
2018-02-21 10:21:52 +0000

    Create and render Card component

---
+++ dog_pairs/src/components/Card.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Card = () => {
+  return (
+    <p>I am a Card</p>
+  )
+}
+
+export default Card;
--- dog_pairs/src/components/CardGrid.js
+++ dog_pairs/src/components/CardGrid.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import Card from './Card';
 
 const CardGrid = () => {
-  return <p>Hello</p>
+  return <Card />
 }
 
 export default CardGrid;
