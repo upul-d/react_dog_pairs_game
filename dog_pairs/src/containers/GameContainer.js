af928d32ced20d37f71cb36f78de6815d9d56e0f
upuld <u.dissanayaka@gmail.com>
2018-02-21 10:37:07 +0000

    Set state working with dogs data

--- dog_pairs/src/containers/GameContainer.js
+++ dog_pairs/src/containers/GameContainer.js
@@ -3,17 +3,26 @@ import CardGrid from '../components/CardGrid';
 
 class GameContainer extends Component {
 
+  constructor() {
+    super();
+
+    this.state = {
+      dogsImages: []
+    }
+  }
+
   componentDidMount() {
     const url = 'https://dog.ceo/api/breed/retriever/images';
     const xhr = new XMLHttpRequest();
 
     xhr.open('GET', url);
 
-    xhr.addEventListener('load', function(){
+    xhr.addEventListener('load', () => {
       const jsonString = xhr.responseText;
       const data = JSON.parse(jsonString);
-      // console.log(data);
       const firstTwentyData = data.message.slice(0, 20);
+
+      this.setState({dogsImages: firstTwentyData});
     })
 
     xhr.send();
