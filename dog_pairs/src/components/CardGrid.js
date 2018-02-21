d1504ec47435e045da8cbf160cd3d72f91bd9f9d
upuld <u.dissanayaka@gmail.com>
2018-02-21 11:14:50 +0000

    Card grid renders cards

--- dog_pairs/src/components/CardGrid.js
+++ dog_pairs/src/components/CardGrid.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import Card from './Card';
 
-const CardGrid = () => {
-  return <Card />
+const CardGrid = (props) => {
+  const cards = props.images.map((image, index) => {
+    return <Card url={image} key={index} />
+  })
+
+  return <div> {cards} </div>
 }
 
 export default CardGrid;
--- dog_pairs/src/containers/GameContainer.js
+++ dog_pairs/src/containers/GameContainer.js
@@ -26,7 +26,7 @@ class GameContainer extends Component {
 
   render() {
     return (
-      <CardGrid />
+      <CardGrid images={this.state.dogsImages}/>
     );
   }
 }
