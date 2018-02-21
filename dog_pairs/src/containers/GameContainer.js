1d3a18ef5892a05c113a2faeac6eb272498b6915
upuld <u.dissanayaka@gmail.com>
2018-02-21 10:32:52 +0000

    Make request to dog api

--- dog_pairs/src/containers/GameContainer.js
+++ dog_pairs/src/containers/GameContainer.js
@@ -2,6 +2,23 @@ import React, { Component } from 'react';
 import CardGrid from '../components/CardGrid';
 
 class GameContainer extends Component {
+
+  componentDidMount() {
+    const url = 'https://dog.ceo/api/breed/retriever/images';
+    const xhr = new XMLHttpRequest();
+
+    xhr.open('GET', url);
+
+    xhr.addEventListener('load', function(){
+      const jsonString = xhr.responseText;
+      const data = JSON.parse(jsonString);
+      // console.log(data);
+      const firstTwentyData = data.message.slice(0, 20);
+    })
+
+    xhr.send();
+  }
+
   render() {
     return (
       <CardGrid />
